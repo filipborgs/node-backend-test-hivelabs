35c9445948f0f71a41ba0e22c240b8a178eaba7e
Filipe Borges <filipborgs48@gmail.com>
2021-08-09 13:04:48 -0300

    refactor: adapt controller responses to http helpers

--- src/controllers/user.search.controller.ts
+++ src/controllers/user.search.controller.ts
@@ -1,3 +1,4 @@
+import { ok } from '../helpers/http-helpers'
 import Controller from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { UserRepository } from '../repository/user.repository'
@@ -5,10 +6,7 @@ import { UserRepository } from '../repository/user.repository'
 export class UserSearchController implements Controller {
   public async handle ({ query: { search } }: HttpRequest): Promise<HttpResponse> {
     const repository = new UserRepository()
-    const arr = await repository.all(search || '')
-    return {
-      statusCode: 200,
-      body: arr
-    }
+    const users = await repository.all(search || '')
+    return ok(users)
   }
 }
