ce532136ee055dc6b6489dd4b77437f01b3c210e
Filipe Borges <filipborgs48@gmail.com>
2021-08-09 12:54:51 -0300

    feat: add http response helpers

---
+++ src/helpers/http-helpers.ts
@@ -0,0 +1,43 @@
+import { HttpResponse } from '../protocols/http'
+
+export const ok = (content: any): HttpResponse => {
+  return {
+    statusCode: 200,
+    body: content
+  }
+}
+
+export const created = (content: any): HttpResponse => {
+  return {
+    statusCode: 201,
+    body: content
+  }
+}
+
+export const noContent = (): HttpResponse => {
+  return {
+    statusCode: 204,
+    body: null
+  }
+}
+
+export const badRequest = (error: Error): HttpResponse => {
+  return {
+    statusCode: 400,
+    body: error.message
+  }
+}
+
+export const notFound = (error: Error): HttpResponse => {
+  return {
+    statusCode: 404,
+    body: error.message
+  }
+}
+
+export const conflict = (): HttpResponse => {
+  return {
+    statusCode: 409,
+    body: null
+  }
+}
