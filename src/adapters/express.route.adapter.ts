739f2285e2447fbdbee472933e83d0f61d414f22
Filipe Borges <filipborgs48@gmail.com>
2021-08-09 18:51:31 -0300

    feat: add request validation

--- src/adapters/express.route.adapter.ts
+++ src/adapters/express.route.adapter.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from 'express'
+import { validationResult } from 'express-validator'
 import Controller from '../protocols/controller'
 
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.mapped())
+    }
+
     const request = {
       body: { ...req.body },
       params: { ...req.params },
